efe8bf3124d395c0ef730bca49bd0df91f35961a
ziwoas <robert@rocu.de>
2026-04-21 17:29:19 +0000

    Add ShellyClient with error normalization

---
+++ lib/shelly_client.rb
@@ -0,0 +1,49 @@
+require "net/http"
+require "json"
+require "uri"
+
+class ShellyClient
+  class Error < StandardError; end
+
+  Reading = Struct.new(:apower_w, :aenergy_wh, keyword_init: true)
+
+  NETWORK_ERRORS = [
+    Net::OpenTimeout, Net::ReadTimeout, Net::WriteTimeout,
+    Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Errno::ENETUNREACH,
+    Errno::ETIMEDOUT, SocketError, EOFError,
+  ].freeze
+
+  def initialize(timeout: 2)
+    @timeout = timeout
+  end
+
+  def fetch(host)
+    uri = URI("http://#{host}/rpc/Switch.GetStatus?id=0")
+    response = get(uri)
+    raise Error, "HTTP #{response.code} from #{host}" unless response.is_a?(Net::HTTPSuccess)
+
+    parse(response.body, host)
+  rescue *NETWORK_ERRORS => e
+    raise Error, "#{e.class}: #{e.message}"
+  end
+
+  private
+
+  def get(uri)
+    Net::HTTP.start(uri.host, uri.port,
+                    open_timeout: @timeout, read_timeout: @timeout) do |http|
+      http.request(Net::HTTP::Get.new(uri.request_uri))
+    end
+  end
+
+  def parse(body, host)
+    data = JSON.parse(body)
+    apower  = data["apower"]
+    aenergy = data.dig("aenergy", "total")
+    raise Error, "missing apower/aenergy from #{host}" if apower.nil? || aenergy.nil?
+
+    Reading.new(apower_w: apower.to_f, aenergy_wh: aenergy.to_f)
+  rescue JSON::ParserError => e
+    raise Error, "invalid JSON from #{host}: #{e.message}"
+  end
+end
---
+++ test/test_shelly_client.rb
@@ -0,0 +1,47 @@
+require "test_helper"
+require "shelly_client"
+require "json"
+
+class ShellyClientTest < Minitest::Test
+  def setup
+    @client = ShellyClient.new(timeout: 2)
+  end
+
+  def shelly_response
+    { "id" => 0, "apower" => 342.5, "aenergy" => { "total" => 12_345.67 } }.to_json
+  end
+
+  def test_parses_successful_response
+    stub_request(:get, "http://192.168.1.192/rpc/Switch.GetStatus?id=0")
+      .to_return(status: 200, body: shelly_response, headers: { "Content-Type" => "application/json" })
+
+    reading = @client.fetch("192.168.1.192")
+    assert_in_delta 342.5, reading.apower_w
+    assert_in_delta 12_345.67, reading.aenergy_wh
+  end
+
+  def test_raises_on_non_200
+    stub_request(:get, /.*/).to_return(status: 503, body: "boot")
+    assert_raises(ShellyClient::Error) { @client.fetch("192.168.1.192") }
+  end
+
+  def test_raises_on_timeout
+    stub_request(:get, /.*/).to_timeout
+    assert_raises(ShellyClient::Error) { @client.fetch("192.168.1.192") }
+  end
+
+  def test_raises_on_connection_refused
+    stub_request(:get, /.*/).to_raise(Errno::ECONNREFUSED)
+    assert_raises(ShellyClient::Error) { @client.fetch("192.168.1.192") }
+  end
+
+  def test_raises_on_malformed_json
+    stub_request(:get, /.*/).to_return(status: 200, body: "not json")
+    assert_raises(ShellyClient::Error) { @client.fetch("192.168.1.192") }
+  end
+
+  def test_raises_on_missing_fields
+    stub_request(:get, /.*/).to_return(status: 200, body: "{}")
+    assert_raises(ShellyClient::Error) { @client.fetch("192.168.1.192") }
+  end
+end
