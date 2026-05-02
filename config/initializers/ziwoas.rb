9c4c5eace36b8aacb266ebcea3473c6aa192f355
Robert Curth <soft.key8170@rocu.de>
2026-05-02 12:49:04 +0200

    Remove data volume.

--- config/initializers/ziwoas.rb
+++ config/initializers/ziwoas.rb
@@ -1,11 +1,9 @@
-require "fileutils"
 require "ziwoas"
 
 Rails.application.config.after_initialize do
   next if ENV["SKIP_BACKGROUND"]
 
   if (defined?(Rails::Server) || defined?(Puma)) && !Rails.env.test?
-    FileUtils.mkdir_p(Rails.root.join("data"))
     config_path = Rails.root.join("config", "ziwoas.yml").to_s
 
     unless Rails.application.ziwoas_app
