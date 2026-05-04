8ec1a9718e5bb76ef9a81494d1934df3c23cce5d
Robert Curth <robert@rocu.de>
2026-05-04 14:44:55 +0000

    Run aggregator as scheduled job

---
+++ app/jobs/aggregator_job.rb
@@ -0,0 +1,18 @@
+require "aggregator"
+require "config_loader"
+require "tzinfo"
+
+class AggregatorJob < ApplicationJob
+  queue_as :default
+
+  def perform(today: Date.today, backup_dir: Rails.root.join("storage", "backup").to_s)
+    config = ConfigLoader.load(Rails.root.join("config", "ziwoas.yml").to_s)
+    tz = TZInfo::Timezone.get(config.timezone)
+    aggregator = Aggregator.new(timezone: tz)
+
+    Rails.logger.info("aggregator: starting scheduled run")
+    aggregator.run_once(today: today)
+    aggregator.backup!(backup_dir)
+    Rails.logger.info("aggregator: done")
+  end
+end
