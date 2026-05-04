1f775605c3d98ab60196aecc6d7f01cc9f1504ea
Robert Curth <robert@rocu.de>
2026-05-04 16:54:16 +0000

    Show weather icon on dashboard

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -5,7 +5,6 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
     get "/"
     assert_response :ok
 
-    assert_select "image[x='184'][y='55'][width='32'][height='32']", 1
     assert_select "text[data-dashboard-target='efPvW'][x='200'][y='102'][text-anchor='middle']", 1
 
     assert_select "image[x='42'][y='150'][width='32'][height='32']", 1
@@ -13,5 +12,26 @@ class DashboardControllerTest < ActionDispatch::IntegrationTest
 
     assert_select "image[x='326'][y='150'][width='32'][height='32']", 1
     assert_select "text[data-dashboard-target='efConsumerW'][x='342'][y='197'][text-anchor='middle']", 1
+  end
+
+  test "uses current weather icon in hero and pv energy flow node" do
+    WeatherRecord.delete_all
+    WeatherRecord.create!(kind: "current", lat: 52.52, lon: 13.405, timestamp: Time.zone.parse("2026-05-04 12:00"), daytime: "night", icon: "cloudy")
+
+    get "/"
+    assert_response :ok
+
+    assert_select "img.hero-icon[src*='weather_cloudy_night']", 1
+    assert_select "image[href*='weather_cloudy_night'][x='184'][y='55'][width='32'][height='32']", 1
+  end
+
+  test "falls back to sun icon without current weather" do
+    WeatherRecord.delete_all
+
+    get "/"
+    assert_response :ok
+
+    assert_select "img.hero-icon[src*='icon_sonne']", 1
+    assert_select "image[href*='icon_sonne'][x='184'][y='55'][width='32'][height='32']", 1
   end
 end
