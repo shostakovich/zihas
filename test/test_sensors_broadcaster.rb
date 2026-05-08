be7ed518a42bd780fac0e3cac9039a88d5f7fec5
Robert Curth <soft.key8170@rocu.de>
2026-05-08 20:21:17 +0200

    Test SensorsBroadcaster also calls WeatherBroadcaster and skips when unconfigured

--- test/test_sensors_broadcaster.rb
+++ test/test_sensors_broadcaster.rb
@@ -10,4 +10,29 @@ class SensorsBroadcasterTest < ActiveSupport::TestCase
     end
     assert_includes calls, [ "sensors", "sensors_dashboard" ]
   end
+
+  test "refresh re-broadcasts the weather current frame" do
+    called = false
+    WeatherBroadcaster.stub(:broadcast_current, -> { called = true }) do
+      Turbo::StreamsChannel.stub(:broadcast_replace_to, ->(*args, **kw) {}) do
+        SensorsBroadcaster.refresh
+      end
+    end
+    assert called, "expected WeatherBroadcaster.broadcast_current to be invoked"
+  end
+
+  test "refresh is a no-op when no sensors are configured" do
+    fake_config = Struct.new(:switchbot, :sensors).new(nil, [])
+    sensor_calls  = 0
+    weather_calls = 0
+    SensorsBroadcaster.stub(:load_config, fake_config) do
+      Turbo::StreamsChannel.stub(:broadcast_replace_to, ->(*args, **kw) { sensor_calls += 1 }) do
+        WeatherBroadcaster.stub(:broadcast_current, -> { weather_calls += 1 }) do
+          SensorsBroadcaster.refresh
+        end
+      end
+    end
+    assert_equal 0, sensor_calls
+    assert_equal 0, weather_calls
+  end
 end
