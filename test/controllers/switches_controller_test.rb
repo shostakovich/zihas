16fc00aebc9f254ee3f370a446ef9850071a3f12
Robert Curth <soft.key8170@rocu.de>
2026-06-26 13:36:30 +0200

    Rewrite lamp list tile to mirror plug card (plush knob right, links to detail)

--- test/controllers/switches_controller_test.rb
+++ test/controllers/switches_controller_test.rb
@@ -6,6 +6,18 @@ class SwitchesControllerTest < ActionDispatch::IntegrationTest
     PlugState.delete_all
     SwitchCommand.delete_all
     Sample.delete_all
+    Light.delete_all
+    @light = Light.create!(key: "ABCDEF01", name: "Wohnzimmer Stehlampe")
+    LightState.record_state(@light.key, on: true, brightness: 60, color_temp_k: 2700)
+  end
+
+  test "lamp tile links to the detail page and exposes a toggle knob" do
+    get switches_url
+    assert_response :success
+    assert_select "a.sw-light-link[href=?]", light_path(@light.key)
+    assert_select ".sw-light-card[data-light-key=?] button.sw-knob", @light.key
+    assert_match "Wohnzimmer Stehlampe", @response.body
+    assert_match "An · Weiß · 60 %", @response.body
   end
 
   test "GET /switches lists only switchable plugs" do
